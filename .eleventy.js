0d120adb61e25b303e477ce25137b9979687c8c8
hadrienmp <github@hadrienmp.fr>
2022-11-04 11:53:02 +0100

    🐛 nav again

--- .eleventy.js
+++ .eleventy.js
@@ -8,8 +8,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection("navigation", function (collectionApi) {
     return collectionApi.getFilteredByTag("page").sort((a, b) => {
       if (a.url === '/') return -1
-      if (b.url === '/') return -1
-      return a.title.localeCompare(b.title)
+      if (b.url === '/') return 1
+      return a.title - (b.title)
     });
   });
   return {
