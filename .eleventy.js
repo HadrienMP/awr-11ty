5c94d9b9276d8323b142625f59e3868988619fa3
hadrienmp <github@hadrienmp.fr>
2022-11-04 11:47:01 +0100

    🐛 attempt to fix navigation sort

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({"_build": "public"});
-  eleventyConfig.addPassthroughCopy({"public": "public"});
+  eleventyConfig.addPassthroughCopy({ "_build": "public" });
+  eleventyConfig.addPassthroughCopy({ "public": "public" });
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
     delimiters: ['//---', '---//']
+  });
+  eleventyConfig.addCollection("navigation", function (collectionApi) {
+    return collectionApi.getFilteredByTag("page").sort((a, b) => {
+      if (a.url === '/') return -1
+      if (b.url === '/') return -1
+      return a - b
+    });
   });
   return {
     dir: {
